039b5a30f36f31b88180519528f32195cefe0a60
aboyce <adam@aboyce.uk>
2022-08-21 13:08:29 +0100

    Moved on_attach to separate function.

--- nvim/lua/aboyce/lsp.lua
+++ nvim/lua/aboyce/lsp.lua
@@ -1,21 +1,33 @@
 local lspconfig = require('lspconfig')
 local capabilities = require('cmp_nvim_lsp').update_capabilities(vim.lsp.protocol.make_client_capabilities())
 
+-- Add keyboard mappings after the language server attaches to the current buffer
+local on_attach = function(client, bufnr)
+
+    local bufopts = { buffer = bufnr }
+
+    vim.keymap.set("n", "K", vim.lsp.buf.hover, bufopts)
+
+    vim.keymap.set("n", "<leader>r", vim.lsp.buf.rename, bufopts)
+
+    vim.keymap.set("n", "gd", vim.lsp.buf.definition, bufopts)
+    vim.keymap.set("n", "gtd", vim.lsp.buf.type_definition, bufopts)
+    vim.keymap.set("n", "gi", vim.lsp.buf.implementation, bufopts)
+
+    vim.keymap.set("n", "<leader>dn", vim.diagnostic.goto_next, bufopts)
+    vim.keymap.set("n", "<leader>dp", vim.diagnostic.goto_prev, bufopts)
+    vim.keymap.set("n", "<leader>dl", "<cmd>Telescope diagnostics<cr>", bufopts)
+
+    vim.keymap.set("n", "<leader>ca", vim.lsp.buf.code_action, bufopts)
+
+end
+
+local lsp_flags = {
+    debounce_text_changes = 150
+}
+
 lspconfig.gopls.setup {
     capabilities = capabilities,
-    on_attach = function(client)
-        vim.keymap.set("n", "K", vim.lsp.buf.hover, { buffer = 0 })
-
-        vim.keymap.set("n", "<leader>r", vim.lsp.buf.rename, { buffer = 0 })
-
-        vim.keymap.set("n", "gd", vim.lsp.buf.definition, { buffer = 0 })
-        vim.keymap.set("n", "gtd", vim.lsp.buf.type_definition, { buffer = 0 })
-        vim.keymap.set("n", "gi", vim.lsp.buf.implementation, { buffer = 0 })
-
-        vim.keymap.set("n", "<leader>dn", vim.diagnostic.goto_next, { buffer = 0 })
-        vim.keymap.set("n", "<leader>dp", vim.diagnostic.goto_prev, { buffer = 0 })
-        vim.keymap.set("n", "<leader>dl", "<cmd>Telescope diagnostics<cr>", { buffer = 0 })
-
-        vim.keymap.set("n", "<leader>ca", vim.lsp.buf.code_action, { buffer = 0 })
-    end,
+    on_attach = on_attach,
+    flags = lsp_flags
 }
